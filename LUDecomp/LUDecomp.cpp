05e8473872cf70fd7d9202eb1386b58df3035764
Richard Song <richard.song.qc@gmail.com>
2014-09-29 23:57:48 -0400

    the subscript start from 0 not 1

--- LUDecomp/LUDecomp.cpp
+++ LUDecomp/LUDecomp.cpp
@@ -2,51 +2,56 @@
 
 void main()
 {
-	float a[10][10] = { 0 }, l[10][10] = { 0 }, u[10][10] = { 0 }, z[10] = { 0 }, x[10] = { 0 }, b[10] = { 0 };
-	int i = 0, j = 0, k = 0, n = 0;
-	printf("\nEnter the size of the coeficient matrix ");
-	scanf("%d", &n);
+	float a[3][3] = { 4, 2, 1, 2, 5, -2, 1, -2, 7 };
+	float l[3][3] = { 0 }; 
+	float u[3][3] = { 0 }; 
+	float z[3] = { 0 };
+	float x[3] = { 0 } ;
+	float b[3] = { 3, 4, 5 };
+	int i = 0, j = 0, k = 0, n = 3;
+	printf("\nEnter the size of the coeficient matrix : %d\n", n);
 	printf("Enter the elements rowwise \n");
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 1; j <= n; j++)
+		for (j = 0; j < n; j++)
 		{
-			scanf("%f", &a[i][j]);
+			printf("%.2f\t", a[i][j]);
 		}
+		printf("\n");
 	}
 
 	printf("Enter the right hand vector\n");
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		scanf("%f", &b[i]);
+		printf("%.2f\t", b[i]);
 	}
 
 	// computations of L and U matrices
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		l[i][1] = a[i][1];
+		l[i][0] = a[i][0];
 	}
 
-	for (j = 2; j <= n; j++)
+	for (j = 1; j < n; j++)
 	{
-		u[1][j] = a[1][j] / l[1][1];
+		u[0][j] = a[0][j] / l[0][0];
 	}
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		u[i][i] = 1;
 	}
 
-	for (i = 2; i <= n; i++)
+	for (i = 1; i < n; i++)
 	{
-		for (j = 2; j <= n; j++)
+		for (j = 1; j < n; j++)
 		{
 			if (i >= j)
 			{
 				l[i][j] = a[i][j];
-				for (k = 1; k <= j - 1; k++)
+				for (k = 0; k <= j - 1; k++)
 				{
 					l[i][j] -= l[i][k] * u[k][j];
 				}
@@ -54,35 +59,36 @@ void main()
 			else
 			{
 				u[i][j] = a[i][j];
-				for (k = 1; k <= i - 1; k++)
+				for (k = 0; k <= i - 1; k++)
 				{
 					u[i][j] -= l[i][k] * u[k][j];
-					u[i][j] /= l[i][i];
+					
 				}
+				u[i][j] /= l[i][i];
 			}
 		}
 	}
 
 	printf("\nThe lower triangular matrix L\n");
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 0; j <= i; j++)
 		{
-			printf("%.2f ", l[i][j]);
+			printf("%.2f\t", l[i][j]);
 		}
 		printf("\n");
 	}
 
 	printf("\nThe upper triangular matrix U\n");
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = 1; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
-			printf("      " );
+			printf("\t" );
 		}
 
-		for (j = i; j <= n; j++)
+		for (j = i; j < n; j++)
 		{
 			printf("%.2f ", u[i][j]);
 		}
@@ -91,11 +97,11 @@ void main()
 	}
 
 	// solve lz=b by forward substitution
-	z[1] = b[1] / l[1][1];
-	for (i = 2; i <= n; i++)
+	z[0] = b[0] / l[0][0];
+	for (i = 1; i < n; i++)
 	{
 		z[i] = b[i];
-		for (j = 1; j <= i - 1; j++)
+		for (j = 0; j <= i - 1; j++)
 		{
 			z[i] -= l[i][j] * z[j];
 		}
@@ -104,17 +110,17 @@ void main()
 
 	//solve Ux=z by backward substitution 
 	x[n] = z[n];
-	for (i = n - 1; i >= 1; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
 		x[i] = z[i];
-		for (j = i + 1; j <= n; j++)
+		for (j = i + 1; j < n; j++)
 		{
 			x[i] -= u[i][j] * x[j];
 		}
 	}
 
 	printf("The solution is ");
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%f ", x[i]);
 	}
