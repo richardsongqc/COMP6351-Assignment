518992f5e5a52b58b6296d7ac9e1751bb0bef342
Richard Song <richard.song.qc@gmail.com>
2014-11-26 01:08:40 -0500

    lamda = 2 and deltaT = 0.001

--- Problem4/prob4.cpp
+++ Problem4/prob4.cpp
@@ -27,8 +27,12 @@ double GetIntegral(CVector v, int N)
 void main()
 {
 	int N = 40, i = 0;
-	CVector u(N + 1), bu(N+1);
-	double dblLamda = 0;
+	double h = ((double)1) / ((double)N);
+	double h2 = pow(h, 2);
+	CVector u(N + 1), bu(N+1), pu(N+1);
+	double t = 0;
+	double deltaT = pow(0.1,3);
+	double dblLamda = 2;
 
 	std::ofstream ofile;
 	ofile.open("prob4.csv");
@@ -45,13 +49,14 @@ void main()
 	// Prepare the vectors of the discretized boundary value problem
 	double dbl = pow(N, 2);
 
-	for (i = 0; i < N+1; i++)
+	for (i = 1; i < N+1; i++)
 	{
-		u[i] = 0;// (double)i / (double)N;
+		u[i] = 0; //(double)i / (double)N;
 	}
 
-	double deltaLamda = 1;
-	for (dblLamda = 0; dblLamda < 5; dblLamda += deltaLamda)
+	pu = u;
+
+	for (t = 0; t < 5; t += deltaT)
 	{
 		int k = 0;
 		int nDim = N - 1;
@@ -60,7 +65,7 @@ void main()
 		CVector l3(nDim);
 		CVector b(nDim), deltaU(nDim), g(nDim);
 
-		printf("%.15f) \n", dblLamda);
+		printf("%.15f) \n", t);
 
 		try
 		{
@@ -69,43 +74,45 @@ void main()
 			{
 				u[N] = 0;
 
-
 				for (i = 0; i < nDim; i++)
 				{
 					double u0 = 0;
 					double u1 = 0;
 					double u2 = 0;
+					double pu1 = 0;
 
 					if (i == 0)
 					{
 						u0 = u[i];
 						u1 = u[i + 1];
 						u2 = u[i + 2];
+						pu1 = pu[i + 1];
 					}
 					else if (i + 2 <= nDim)
 					{
 						u0 = u[i];
 						u1 = u[i + 1];
 						u2 = u[i + 2];
+						pu1 = pu[i + 1];
 					}
 					else if (i + 1 == nDim)
 					{
 						u0 = u[nDim - 1];
 						u1 = u[nDim];
 						u2 = u[N];
+						pu1 = pu[nDim];
 					}
 
-					b[i] = -dbl* (u0 - 2 * u1 + u2) - dblLamda*(exp(u0) + 10 * exp(u1) + exp(u2)) / ((double)12);
+					b[i] = deltaT*u0 / h2 - (1 + 2 * deltaT / h2)*u1 + deltaT*u2 / h2 + pu1 + deltaT*dblLamda*exp(u1);
 
-					double d0 = dbl + dblLamda*(exp(u0) / ((double)12));
-					double d1 = -2 * dbl + 10 * dblLamda* (exp(u1) / ((double)12));
-					double d2 = dbl + dblLamda* (exp(u2) / ((double)12));
+					double d0 = -deltaT / h2;
+					double d1 = 1 + 2 * deltaT / h2 - deltaT*dblLamda*exp(u1);
 
 					l2[i] = d1;
 
 					if (i > 0)
 					{
-						l3[i - 1] = d2;
+						l3[i - 1] = d0;
 					}
 
 					if (i + 1 < nDim)
@@ -130,6 +137,7 @@ void main()
 					//ofile << std::setw(25) << u[i + 1] << ",";
 				}
 
+
 				if (deltaU < 0.00000000001)
 				{
 					printf("\nk = %d\n", k);
@@ -146,19 +154,21 @@ void main()
 		{
 			cout << "*****************************************************************" << endl;
 			cout << "\t!!!Lamda = " << dblLamda << "!!!" << endl;
-			dblLamda -= deltaLamda;
-			deltaLamda /= (double)10;
-			u = bu;
-			if (deltaLamda < pow(0.1,15))
-			{
-				printf("\n============================================================\nLamda = %.15f\n", dblLamda);
-				break;
-			}
+			//dblLamda -= deltaLamda;
+			//deltaLamda /= (double)10;
+			//u = bu;
+			//if (deltaLamda < pow(0.1,15))
+			//{
+			//	printf("\n============================================================\nLamda = %.15f\n", dblLamda);
+			//	break;
+			//}
 			continue;
 		}
 
-		printf("\nLamda = %.15f \t delta = %.15f\n", dblLamda, deltaLamda);
-		ofile << std::setprecision(17) << dblLamda << ",";
+		pu = u;
+
+		printf("\tT = %.15f \t deltaT = %.15f\n", t, deltaT);
+		ofile << std::setprecision(17) << t << ",";
 		for (i = 0; i < N; i++)
 		{
 			ofile << std::setw(15) << u[i] << ",";
