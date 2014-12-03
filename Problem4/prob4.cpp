7e0f2ebb39596b201a27eb82ca055d7a266cf1c6
Richard Song <richard.song.qc@gmail.com>
2014-12-02 21:01:05 -0500

    Explicit Euler Method
    Bratu

--- Problem4/prob4.cpp
+++ Problem4/prob4.cpp
@@ -26,12 +26,12 @@ double GetIntegral(CVector v, int N)
 
 void main()
 {
-	int N = 40, i = 0;
+	int N = 100, i = 0;
 	double h = ((double)1) / ((double)N);
 	double h2 = pow(h, 2);
-	CVector u(N + 1), bu(N+1), pu(N+1);
+	CVector u(N + 1), bu(N + 1) ;
 	double t = 0;
-	double deltaT = pow(0.1,3);
+	double deltaT = pow(0.1, 4);
 	double dblLamda = 2;
 
 	std::ofstream ofile;
@@ -49,14 +49,12 @@ void main()
 	// Prepare the vectors of the discretized boundary value problem
 	double dbl = pow(N, 2);
 
-	for (i = 1; i < N+1; i++)
+	for (i = 1; i < N + 1; i++)
 	{
 		u[i] = 0; //(double)i / (double)N;
 	}
 
-	pu = u;
-
-	for (t = 0; t < 5; t += deltaT)
+	for (t = 0; t < 1; t += deltaT)
 	{
 		int k = 0;
 		int nDim = N - 1;
@@ -67,105 +65,51 @@ void main()
 
 		printf("%.15f) \n", t);
 
+		u[N] = 0;
+
 		try
 		{
-			
-			do
+			for (i = 0; i < nDim; i++)
 			{
-				u[N] = 0;
+				double u0 = 0;
+				double u1 = 0;
+				double u2 = 0;
+				double pu1 = 0;
 
-				for (i = 0; i < nDim; i++)
+				if (i == 0)
 				{
-					double u0 = 0;
-					double u1 = 0;
-					double u2 = 0;
-					double pu1 = 0;
-
-					if (i == 0)
-					{
-						u0 = u[i];
-						u1 = u[i + 1];
-						u2 = u[i + 2];
-						pu1 = pu[i + 1];
-					}
-					else if (i + 2 <= nDim)
-					{
-						u0 = u[i];
-						u1 = u[i + 1];
-						u2 = u[i + 2];
-						pu1 = pu[i + 1];
-					}
-					else if (i + 1 == nDim)
-					{
-						u0 = u[nDim - 1];
-						u1 = u[nDim];
-						u2 = u[N];
-						pu1 = pu[nDim];
-					}
-
-					b[i] = deltaT*u0 / h2 - (1 + 2 * deltaT / h2)*u1 + deltaT*u2 / h2 + pu1 + deltaT*dblLamda*exp(u1);
-
-					double d0 = -deltaT / h2;
-					double d1 = 1 + 2 * deltaT / h2 - deltaT*dblLamda*exp(u1);
-
-					l2[i] = d1;
-
-					if (i > 0)
-					{
-						l3[i - 1] = d0;
-					}
-
-					if (i + 1 < nDim)
-					{
-						l1[i + 1] = d0;
-					}
+					u0 = u[i];
+					u1 = u[i + 1];
+					u2 = u[i + 2];
+				}
+				else if (i + 2 <= nDim)
+				{
+					u0 = u[i];
+					u1 = u[i + 1];
+					u2 = u[i + 2];
+				}
+				else if (i + 1 == nDim)
+				{
+					u0 = u[nDim - 1];
+					u1 = u[nDim];
+					u2 = u[N];
 				}
 
-				// Apply LU-Decomp to solve it
-				LUDecompTridiagnoal(l1, l2, l3, b, deltaU, g);
-
-				//printf("\nk = %d\n", k);
-				for (i = 0; i < nDim; i++)
+				bu[i+1] = u1 + deltaT *(u0 - 2 * u1 + u2) / h2 + deltaT*dblLamda*exp(u1);
+				if (_finite(bu[i]) == 0)
 				{
-					u[i + 1] = deltaU[i] + u[i + 1];
-					if (_finite(u[i + 1]) == 0)
-					{
-						throw MyException();
-					}
-					//printf("deltaU[%d]=%15.11f\tu[%d]=%12.11f\n", i, deltaU[i], i, u[i+1] );
-					// Output the results to one excel file, later we can analyze this file.
-					//ofile << std::setw(25) << u[i + 1] << ",";
+					throw MyException();
 				}
 
-
-				if (deltaU < 0.00000000001)
-				{
-					printf("\nk = %d\n", k);
-					break;
-				}
-
-				//printf("\n");
-
-				k++;
-			} 
-			while (k < 1000);
+			}
 		}
 		catch (MyException & e)
 		{
 			cout << "*****************************************************************" << endl;
-			cout << "\t!!!Lamda = " << dblLamda << "!!!" << endl;
-			//dblLamda -= deltaLamda;
-			//deltaLamda /= (double)10;
-			//u = bu;
-			//if (deltaLamda < pow(0.1,15))
-			//{
-			//	printf("\n============================================================\nLamda = %.15f\n", dblLamda);
-			//	break;
-			//}
-			continue;
+			break;
 		}
 
-		pu = u;
+		u = bu;
 
 		printf("\tT = %.15f \t deltaT = %.15f\n", t, deltaT);
 		ofile << std::setprecision(17) << t << ",";
@@ -173,9 +117,7 @@ void main()
 		{
 			ofile << std::setw(15) << u[i] << ",";
 		}
-		ofile << "0," << GetIntegral(u,N) << std::endl;
-
-		bu = u;
+		ofile << "0," << GetIntegral(u, N) << std::endl;
 	}
 
 	ofile.close();
